8608f58a31feea606ef35c416311e80121a3dba4
Pilla Sai Prasad <psairockz@gmail.com>
2023-12-15 12:57:46 +0530

    added dropdown to declarative posts to filter data

--- src/app/pages/declarative-posts/declarative-posts.component.ts
+++ src/app/pages/declarative-posts/declarative-posts.component.ts
@@ -1,16 +1,39 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Subject, combineLatest, map } from 'rxjs';
+import { DeclarativeCategoryService } from 'src/app/services/DeclarativeCatergory.service';
 import { DeclarativePostService } from 'src/app/services/DeclarativePost.service';
 
 @Component({
   selector: 'app-declarative-posts',
   templateUrl: './declarative-posts.component.html',
   styleUrls: ['./declarative-posts.component.scss'],
-  changeDetection:ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DeclarativePostsComponent  {
-  posts$ = this.postService.postWithCategory$
-  constructor(private postService: DeclarativePostService ) {
+export class DeclarativePostsComponent {
+  selectedCategorySubject = new Subject<string>();
+  selectedCategoryActions$ = this.selectedCategorySubject.asObservable();
 
+  posts$ = this.postService.postWithCategory$;
+  categories$ = this.categoryService.categories$;
+  selectedCategoryId = '';
+
+  filteredPosts$ = combineLatest([
+    this.posts$,
+    this.selectedCategoryActions$,
+  ]).pipe(
+    map(([posts, selectedCategoryId]) => {
+      return posts.filter(
+        (post) => post.categoryId === selectedCategoryId
+      );
+    })
+  );
+
+  constructor(
+    private postService: DeclarativePostService,
+    private categoryService: DeclarativeCategoryService
+  ) {}
+  onCategoryChange(event: Event) {
+    let selectedCategoryId = (event.target as HTMLSelectElement).value;
+    this.selectedCategorySubject.next(selectedCategoryId);
   }
-
 }
